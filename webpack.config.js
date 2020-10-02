8b3b8610d5b7729b0741bf275efeb175753e0236
Dinesh Kumar Gnanasekaran <dinesh.gna111@gmail.com>
2020-10-02 20:29:49 +0530

    Production tuning

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'aMaze.bundle.js'
+      filename: 'bundle.js'
   },
   watch: true,
   resolve : {
@@ -29,7 +29,7 @@ module.exports = {
         use: [
             {
               loader: 'file-loader',
-              options: {name: 'aMaze.bundle.css'}
+              options: {name: 'bundle.css'}
             },
             'sass-loader'
         ]
